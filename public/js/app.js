5e37e85144061f7c289584f44974aede2b805c09
apainintheneck <apainintheneck@gmail.com>
2021-01-09 18:20:40 -0800

    feed.ejs now loads posts from the reddit api.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 import reddit from './reddit-api.js';
 
-const searchForm = document.getElementById('search-form');
-const searchBtn = document.getElementById('search-btn');
-const searchInput = document.getElementById('search-input');
-
 searchReddit("tech", "10", "hot"); //Shows latest news when the page is first loaded.
 
 // Truncate String Function
@@ -22,9 +18,9 @@ function searchReddit(searchTerm, searchLimit, sortBy){
     results.forEach(post => {
       output += `
       <article>
-        <h2>${post.title}</h2><br>
-        <p class="card-text">${truncateString(post.selftext, 100)}</p>
-        <a href="${post.url}" target="_blank">Read More</a>
+        <a href="${post.url}" target="_blank">
+          <h3>${truncateString(post.title, 100)}</h3>
+        </a>
       </article>
       `;
     });
